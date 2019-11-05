6faf12eddbd233ea929488408eeceeab828da4f9
FritzJo <fritzjo-git@mailbox.org>
2019-11-05 11:13:02 +0100

    theme switch js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -39,3 +39,16 @@ function redirect(e) {
     }
     window.location = url;
 }
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
